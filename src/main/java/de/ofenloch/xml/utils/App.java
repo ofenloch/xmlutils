c3d1c00b8350c0411131d1ac5331e758bdbba700
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-08-20 20:14:33 +0200

    initial commit right after 'mvn archetype:generate -DgroupId=de.ofenloch.xml.utils -DartifactId=xmlutils -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false'

---
+++ src/main/java/de/ofenloch/xml/utils/App.java
@@ -0,0 +1,13 @@
+package de.ofenloch.xml.utils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
